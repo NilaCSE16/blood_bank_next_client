b4d47efa38e17401d8a61a66862e827286cf675d
NilaCSE16 <112371425+NilaCSE16@users.noreply.github.com>
2024-05-19 10:21:46 +0600

    Add some new functionality

--- src/components/Services/API.ts
+++ src/components/Services/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://blood-bank-server-eight.vercel.app/api/",
 });
 
 API.interceptors.request.use((req) => {
