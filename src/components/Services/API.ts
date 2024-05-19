c3e89648bc7787086f8a33ddfa671f6a8235d7cf
NilaCSE16 <112371425+NilaCSE16@users.noreply.github.com>
2024-05-19 09:36:48 +0600

    change origin

--- src/components/Services/API.ts
+++ src/components/Services/API.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api/" });
+const API = axios.create({
+  baseURL: "https://blood-bank-server-eight.vercel.app/api/",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
